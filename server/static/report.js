cb296a5b945485bef4876028aea0f980566294c4
Alex-Shane <ashane1230@gmail.com>
2023-06-20 13:00:08 -0700

    javascript bug with updating industry options based on exchange

---
+++ server/static/report.js
@@ -0,0 +1,24 @@
+
+
+window.onload = function() {
+    var period = document.getElementById("period")
+    var exchange = document.getElementById("exchange")
+    var industry = document.getElementById("industry")
+}
+
+function setIndustryOptions() {
+    var exchangeObject = {
+        "SP500": ['assets', 'tech'],
+        "NASDAQ": ['just', 'tech'],
+        "DOW": ['small'],
+        "NYSE": ['has', 'everything']
+      }
+    //empty Chapters dropdown
+    industry.length = 1;
+    //display correct values
+    exchangeVal = exchange.value
+    alert(exchangeObject.exchangeVal)
+    for (var i = 0; i < exchangeObject.exchangeVal.length; i++) {
+      industry.options[industry.options.length] = new Option(exchangeObject.exchangeVal[i], exchangeObject.exchangeVal[i]);
+    }
+  }
